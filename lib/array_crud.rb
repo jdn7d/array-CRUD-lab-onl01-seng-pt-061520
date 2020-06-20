38d411af99ebffe837d054aeda05ebee7f0f993a
Jessie Donovan <donovanajessie@gmail.com>
2020-06-20 18:56:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,11 +12,11 @@ array = [1,2,3,4]
 end
 
 def add_element_to_start_of_array(array, element)
-
+array.unshift(6)
 end
 
 def remove_element_from_end_of_array(array)
-
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
